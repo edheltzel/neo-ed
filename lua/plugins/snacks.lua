d59593dff789efb8cdd273ba7676020e0c748d0f
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-11-06 17:10:26 -0500

    adding image support for neovim via snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      image = { enable = true },
       persistance = {
         need = 1,
       },
@@ -24,7 +25,7 @@ return {
         preset = {
           keys = {
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('')" },
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             {
