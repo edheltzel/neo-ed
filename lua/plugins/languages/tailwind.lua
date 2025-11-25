322a48d3ee7411fb0b55baae5e2307f7a181fd00
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-11-25 15:45:36 -0500

    migrating language support from old configs

---
+++ lua/plugins/languages/tailwind.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          settings = {
+            tailwindCSS = {
+              includeLanguages = {
+                elixir = "html-eex",
+                eelixir = "html-eex",
+                heex = "html-eex",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
