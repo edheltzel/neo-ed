8d8bd4e19ad6d4c498b8d03d904ce1d398400ce7
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-12-06 15:13:10 -0500

    eldritch

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
   {
+    -- {
+    --   "rose-pine/neovim",
+    --   name = "rose-pine",
+    -- },
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme options: "eldritch", "rose-pine", "rose-pine-moon", "rose-pine-dawn"
       colorscheme = "eldritch",
-    },
-    {
-      "rose-pine/neovim",
-      name = "rose-pine",
     },
   },
 }
