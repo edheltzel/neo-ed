ea2e093c1a4d2a2c0ec211524b21a64d4fe31459
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-12-19 11:38:32 -0500

    remove ghostty syntax

--- lua/plugins/editor/ghostty.lua
+++
@@ -1,9 +0,0 @@
--- Ghostty terminal config syntax highlighting
--- Uses tree-sitter-ghostty plugin which self-registers with nvim-treesitter
-return {
-  {
-    "bezhermoso/tree-sitter-ghostty",
-    build = "make nvim_install",
-    ft = "ghostty",
-  },
-}
