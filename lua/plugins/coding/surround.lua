9cdee069707854199fb475f7a61eceea54849df1
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-12-22 13:21:49 -0500

    updated mini surrounds for markdown only syntax
    since i am using obsidian.nvim for writing inside of my fieldnotes,
    these mini surround behaviors and keybindings will speed up writing in
    markdown.

--- lua/plugins/coding/surround.lua
+++ lua/plugins/coding/surround.lua
@@ -22,6 +22,59 @@ return {
         input = { "==().-()==" },
         output = { left = "==", right = "==" },
       },
+      -- Use 'L' for markdown links: gsaL, gsdL, gsrL
+      L = {
+        input = { "%[().-()%]%(.-%)" },
+        output = function()
+          local url = MiniSurround.user_input("URL")
+          if not url then
+            return nil
+          end
+          return { left = "[", right = "](" .. url .. ")" }
+        end,
+      },
     },
   },
+  config = function(_, opts)
+    require("mini.surround").setup(opts)
+
+    -- Markdown-specific surroundings: no spaces for brackets
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "markdown", "mdx" },
+      callback = function()
+        vim.b.minisurround_config = {
+          custom_surroundings = {
+            -- Brackets - no spaces
+            ["["] = { output = { left = "[", right = "]" } },
+            ["]"] = { output = { left = "[", right = "]" } },
+            -- Parentheses - no spaces
+            ["("] = { output = { left = "(", right = ")" } },
+            [")"] = { output = { left = "(", right = ")" } },
+            -- Curly braces - no spaces
+            ["{"] = { output = { left = "{", right = "}" } },
+            ["}"] = { output = { left = "{", right = "}" } },
+            -- Angle brackets - no spaces
+            ["<"] = { output = { left = "<", right = ">" } },
+            [">"] = { output = { left = "<", right = ">" } },
+            -- Backticks - for inline code
+            ["`"] = { output = { left = "`", right = "`" } },
+            -- Single quotes
+            ["'"] = { output = { left = "'", right = "'" } },
+            -- Double quotes
+            ['"'] = { output = { left = '"', right = '"' } },
+            -- Asterisks - for emphasis
+            ["*"] = { output = { left = "*", right = "*" } },
+            -- Underscores - for emphasis
+            ["_"] = { output = { left = "_", right = "_" } },
+            -- Tilde - for strikethrough
+            ["~"] = { output = { left = "~~", right = "~~" } },
+            -- Include global custom surroundings
+            e = opts.custom_surroundings.e,
+            h = opts.custom_surroundings.h,
+            L = opts.custom_surroundings.L,
+          },
+        }
+      end,
+    })
+  end,
 }
