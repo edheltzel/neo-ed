bb19bdd21ced3109a2f47583375d703e8f50f12a
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-01-14 07:28:10 -0500

    icon update for snacks dashboard

--- lua/plugins/utils/snacks-dashboard.lua
+++ lua/plugins/utils/snacks-dashboard.lua
@@ -4,12 +4,12 @@ return {
     dashboard = {
       preset = {
         keys = {
-          { icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('recent')" },
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('recent')" },
+          { icon = "󰮗 ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "󱩾 ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           {
-            icon = " ",
+            icon = " ",
             key = "o",
             desc = "Find Session",
             action = function()
@@ -17,14 +17,14 @@ return {
             end,
           },
           { icon = "⊙ ", key = "d", desc = "DOTFILES", action = ":cd ${HOME}/.dotfiles | :e ." },
-          { icon = " ", key = "v", desc = "NOE.ED", action = ":cd ${HOME}/.dotfiles/neoed/.config/nvim | :e ." },
+          { icon = " ", key = "v", desc = "NOE.ED", action = ":cd ${HOME}/.dotfiles/neoed/.config/nvim | :e ." },
           {
-            icon = "󰎞 ",
+            icon = " ",
             key = "n",
             desc = "FieldNotes✱",
-            action = ":cd ${HOME}/Library/Mobile\\ Documents/iCloud~md~obsidian/Documents/FieldNotes✱\\  | :e .",
+            action = ":cd ${HOME}/Library/Mobile\\ Documents/iCloud~md~obsidian/Documents/FieldNotes✱ | :e .",
           },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = "󰩈 ", key = "q", desc = "Quit", action = ":qa" },
         },
