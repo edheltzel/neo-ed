9d6e6c325d5fe83b3dc93e53f822bf4ca23ac007
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-01-25 11:15:42 -0500

    dotfile update to theme switcher, addes rose-pine dawn theme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -6,7 +6,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = true,
+      transparent = false,
       dim_inactive = true,
       styles = {
         sidebars = "dark",
@@ -99,7 +99,7 @@ return {
       --   Aura: "aura-dark", "aura-dark-soft-text", "aura-soft-dark", "aura-soft-dark-soft-text"
       --   Rose Pine: "rose-pine", "rose-pine-moon", "rose-pine-dawn"
       --   Tokyo Night: "tokyonight", "tokyonight-storm", "tokyonight-moon", "tokyonight-day"
-      colorscheme = "eldritch",
+      colorscheme = "rose-pine-dawn",
     },
   },
 }
